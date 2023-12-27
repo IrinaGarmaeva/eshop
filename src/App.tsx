1a08ec440fa9f67c9f5286560c3498d75ec3e4a5
Irina Garmaeva <garmaeva-i@mail.ru>
2023-12-27 17:00:05 +0100

    Groupping imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ROUTES } from './utils/constants';
-import Home from './components/pages/Home/Home';
-import Cart from './components/pages/Cart/Cart';
-import Contact from './components/pages/Contact/Contact';
-import MyOrders from './components/pages/MyOrders/MyOrders';
-import Admin from './components/pages/Admin/Admin';
-import Header from './components/layout/Header/Header';
-import Footer from './components/layout/Footer/Footer';
+import { Header, Footer } from './components/layout';
+import { Home, Cart, Contact, MyOrders, Admin, SignIn, SignUp } from './components/pages';
 import './App.scss';
-import SignIn from './components/pages/SignIn/SignIn';
-import SignUp from './components/pages/SignUp/SignUp';
 
 function App() {
   return (
---
+++ src/components/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from './Home/Home';
+export { default as Cart } from './Cart/Cart';
+export { default as Contact } from './Contact/Contact';
+export { default as MyOrders } from './MyOrders/MyOrders';
+export { default as Admin } from './Admin/Admin';
+export { default as SignIn } from './SignIn/SignIn';
+export { default as SignUp } from './SignUp/SignUp';
